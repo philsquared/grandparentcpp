14b705b8798aae1322c6cec82a34385a3a647deb
Phil nash <github@philnash.me>
2019-06-24 14:36:32 +0100

    First commit

---
+++ memory.cpp
@@ -0,0 +1,182 @@
+#include "catch.hpp"
+#include <memory>
+
+namespace Cpp98 {
+    class MyClass {
+        std::string m_name;
+        std::vector<std::string> m_data;
+    public:
+        MyClass(std::string const& name, std::vector<std::string> const& data) : m_name(name), m_data(data) {}
+
+        std::string name() const { return m_name; }
+        std::string data( int i ) const { return m_data.at(i); }
+
+        size_t size() const { return m_data.size(); }
+    };
+}
+namespace Cpp11 {
+
+    template<typename T>
+    std::unique_ptr<T> make_unique() {
+        return std::unique_ptr<T>( new T );
+    }
+//    template<typename T, typename Arg1>
+//    std::unique_ptr<T> make_unique( Arg1 const& arg1 ) {
+//        return std::unique_ptr<T>( new T( arg1 ) );
+//    }
+//    template<typename T, typename Arg1, typename Arg2>
+//    std::unique_ptr<T> make_unique( Arg1 const& arg1, Arg2 const& arg2 ) {
+//        return std::unique_ptr<T>( new T( arg1, arg2 ) );
+//    }
+//    template<typename T, typename Arg1, typename Arg2, typename Arg3>
+//    std::unique_ptr<T> make_unique( Arg1 const& arg1, Arg2 const& arg2, Arg3 const& arg3 ) {
+//        return std::unique_ptr<T>( new T( arg1, arg2, arg3 ) );
+//    }
+    // ...etc
+
+    // use forwarding refs
+    // don't use move because these are not necessarily rvalues
+    template<typename T, typename Arg1>
+    std::unique_ptr<T> make_unique( Arg1&& arg1 ) {
+        return std::unique_ptr<T>( new T( std::forward<Arg1>( arg1 ) ) );
+    }
+    template<typename T, typename Arg1, typename Arg2>
+    std::unique_ptr<T> make_unique( Arg1&& arg1, Arg2&& arg2 ) {
+        return std::unique_ptr<T>( new T( std::forward<Arg1>( arg1 ), std::forward<Arg2>( arg2 ) ) );
+    }
+    template<typename T, typename Arg1, typename Arg2, typename Arg3>
+    std::unique_ptr<T> make_unique( Arg1&& arg1, Arg2&& arg2, Arg3&& arg3 ) {
+        return std::unique_ptr<T>( new T( std::forward<Arg1>( arg1 ), std::forward<Arg2>( arg2 ), std::forward<Arg3>( arg3 ) ) );
+    }
+    // - perfect forwarding
+
+    // Then use variadic templates
+
+//    template<class T, class... Args>
+//    std::unique_ptr<T> make_unique( Args&&... args ) {
+//        return std::unique_ptr<T>( new T( std::forward<Args>( args )... ) );
+//    }
+
+    class MyClass {
+        std::string m_name;
+        std::vector<std::string> m_data;
+    public:
+        MyClass( std::string name, std::vector<std::string> data )
+                : m_name( std::move( name ) ),
+                  m_data( std::move( data ) ) {}
+
+        MyClass( MyClass &&other )
+        :   m_name( std::move( other.m_name ) ),
+            m_data( std::move( other.m_data ) )
+        {}
+
+        std::string name() const { return m_name; }
+        std::string data( int i ) const { return m_data.at(i); }
+
+        size_t size() const { return m_data.size(); }
+    };
+}
+
+TEST_CASE( "Heap memory management" ) {
+
+    SECTION( "C++98" ) {
+        using namespace Cpp98;
+
+        std::vector<std::string> data;
+        data.push_back("first");
+
+        SECTION( "raw alloc" ) {
+
+            MyClass *obj = new MyClass("Harry", data); // data copied in
+
+            REQUIRE(obj->name() == "Harry");
+            REQUIRE(obj->size() == 1);
+            REQUIRE(obj->data(0) == "first");
+            REQUIRE_THROWS( obj->data(1) );
+
+            delete obj; // leaks if you forget this, or if an exception is thrown
+        }
+        SECTION( "auto_ptr" ) {
+            std::auto_ptr<MyClass> obj( new MyClass("Harry", data) ); // data copied in
+
+            REQUIRE(obj->name() == "Harry");
+            REQUIRE(obj->size() == 1);
+            REQUIRE(obj->data(0) == "first");
+
+            // auto_ptr deletes obj in destructor - even via exception
+
+            // However: -
+            // dangerous to pass around - ownership is _transferred_
+            // can't share ownership
+            // can't use in a std container (because would be dangerous)
+            // - deprecated in C++11, removed in C++17
+
+            // Common alternatives: boost::scoped_ptr, boost::shared_ptr, Loki::SmartPtr
+            // unique ownership a thorny problem in C++98
+        }
+    }
+    SECTION( "C++11" ) {
+        using namespace Cpp11;
+
+        // Now using init list
+        std::vector<std::string> data = { "first", "second" };
+
+        SECTION( "unique_ptr - data copied" ) {
+            std::unique_ptr<MyClass> obj(new MyClass("Harry", data)); // data copied in
+
+            REQUIRE(obj->name() == "Harry");
+            REQUIRE(obj->size() == 2);
+            REQUIRE(obj->data(0) == "first");
+        }
+
+        SECTION( "unique_ptr - data moved" ) {
+            std::unique_ptr<MyClass> obj(new MyClass("Harry", std::move(data)));
+            // data moved in - no longer usable
+            // std::move doesn't actually "move" anything
+            // it just, effectively, "casts" it's arg to an rvalue reference
+            // an rvalue reference can be thought of as an unnamed object - usually a temporary
+            // - also variables in a return statement
+
+            REQUIRE(obj->name() == "Harry");
+            REQUIRE(obj->size() == 2);
+            REQUIRE(obj->data(0) == "first");
+
+            REQUIRE( data.size() == 0 ); // !!! Undefined behaviour - don't do this
+        }
+
+        SECTION( "unique_ptr - temp data moved" ) {
+            std::unique_ptr<MyClass> obj(new MyClass("Harry", {"first", "second"}));
+            REQUIRE(obj->size() == 2);
+        }
+
+        SECTION( "unique_ptr - our own moveable object" ) {
+            MyClass stackObject("Harry", {"first", "second" } );
+            std::unique_ptr<MyClass> obj(new MyClass( std::move( stackObject ) ) );
+            REQUIRE(obj->size() == 2);
+            REQUIRE( stackObject.size() == 0 ); // UB - don't do this
+        }
+
+        SECTION( "shared_ptr" ) {
+            std::shared_ptr<MyClass> obj(new MyClass("Harry", {"first", "second"}));
+            std::shared_ptr<MyClass> obj2 = obj;
+
+            REQUIRE( obj->name() == "Harry" );
+            REQUIRE( obj2->name() == "Harry" );
+            REQUIRE( obj.get() == obj2.get() ); // Same underlying object
+        }
+
+        // It's possible to leak memory if an exception is thrown between an object being allocated
+        // and a unique_ptr or shared_ptr constructed
+
+        SECTION( "make_shared" ) {
+            // can use auto
+            // (but have to specify vector>
+            auto obj = std::make_shared<MyClass>( "Harry", std::vector<std::string>{"first", "second"} );
+        }
+
+        SECTION( "make_unique" ) {
+            auto obj = Cpp11::make_unique<MyClass>( "Harry", std::vector<std::string>{"first", "second"} );
+        }
+
+    }
+}
---
+++ vector-int-string.cpp
@@ -0,0 +1,163 @@
+#include "catch.hpp"
+
+#include <vector>
+#include <numeric>
+#include <sstream>
+
+using namespace Catch::Matchers;
+
+
+TEST_CASE( "Vector of ints to vector of string" ) {
+
+    SECTION( "C++98" ) {
+
+        std::vector<int> fib;
+
+        fib.reserve( 6 );
+
+        fib.push_back( 1 );
+        fib.push_back( 1 );
+        fib.push_back( 2 );
+        fib.push_back( 3 );
+        fib.push_back( 5 );
+        fib.push_back( 8 );
+        // ...
+
+        std::vector<std::string> stringFib;
+        stringFib.reserve( fib.size() );
+
+        std::vector<int>::const_iterator it = fib.begin();
+        std::vector<int>::const_iterator itEnd = fib.end();
+
+        for(; it != itEnd; ++it ) {
+            // No easy way to convert int to string
+            // - could use non-std C, itoa
+            // - or C's sprintf
+            // Here we'll go the "idiomatic" C++(98) way
+            std::stringstream ss;
+            ss << *it;
+            // In general we should really check ss.bad()
+            // and we might need to optimise perf by moving declaration outside the loop
+            // and clear each time
+            // still performs badly
+
+            stringFib.push_back( ss.str() );
+        }
+
+        std::vector<std::string> expected;
+        expected.push_back( "1" );
+        expected.push_back( "1" );
+        expected.push_back( "2" );
+        expected.push_back( "3" );
+        expected.push_back( "5" );
+        expected.push_back( "8" );
+
+        REQUIRE_THAT( stringFib, Equals( expected ) );
+    }
+
+    SECTION( "C++11" ) {
+        // Initializer list
+        std::vector<int> fib = { 1, 1, 2, 3, 5, 8 };
+
+        std::vector<std::string> expected = { "1", "1", "2", "3", "5", "8" };
+
+        // v1
+        SECTION("Use to_string") {
+            std::vector<std::string> stringFib;
+            stringFib.reserve( fib.size() );
+
+            std::vector<int>::const_iterator it = fib.begin();
+            std::vector<int>::const_iterator itEnd = fib.end();
+
+            for(; it != itEnd; ++it ) {
+                stringFib.push_back( std::to_string( *it ) );
+            }
+
+            REQUIRE_THAT( stringFib, Equals(expected) );
+        }
+
+        // v2
+        SECTION("Use auto") {
+
+            std::vector<std::string> stringFib;
+            // reserve?
+
+            auto itEnd = fib.end();
+            for( auto it = fib.begin(); it != itEnd; ++it ) {
+                stringFib.push_back( std::to_string( *it ) );
+            }
+
+            REQUIRE_THAT( stringFib, Equals(expected) );
+        }
+
+        // v2
+        SECTION( "range-based for" ) {
+
+            std::vector<std::string> stringFib;
+            // reserve?
+
+            for ( int i : fib ) {
+                stringFib.push_back( std::to_string( i ) );
+            }
+
+            REQUIRE_THAT( stringFib, Equals(expected) );
+        }
+
+        // v2.5?
+        SECTION( "AAA?" )  {
+            std::vector<std::string> stringFib;
+
+            for ( auto i : fib ) { // Is auto better?
+                stringFib.push_back( std::to_string( i ) );
+            }
+
+            REQUIRE_THAT( stringFib, Equals(expected) );
+        }
+
+        // v3a
+        SECTION( "use algorithm (no lambda)" ) {
+
+            std::vector<std::string> stringFib;
+
+            struct Converter {
+                std::string operator()( int i ) const {
+                    return std::to_string( i );
+                }
+            };
+
+            // can't (easily) pass pointer to to_string
+            std::transform( fib.begin(), fib.end(), std::back_inserter( stringFib ), Converter() );
+            REQUIRE_THAT( stringFib, Equals(expected) );
+        }
+
+        // v3b
+        SECTION( "use algorithm (lambda)" ) {
+
+            std::vector<std::string> stringFib;
+
+            std::transform(
+                    fib.begin(), fib.end(),
+                    std::back_inserter( stringFib ),
+                    []( int i ) { return std::to_string( i ); } );
+
+            REQUIRE_THAT( stringFib, Equals(expected) );
+        }
+
+        // v3c
+        SECTION( "use algorithm (lambda with captures)" ) {
+
+            std::vector<std::string> stringFib;
+
+            std::string prefix = ":-) ";
+            // not automatically captured
+            // can capture specific variables by value, by reference or all by value, by reference (or mix)
+            // references may dangle if lifetime of lambda exceeds
+            std::transform(
+                    fib.begin(), fib.end(),
+                    std::back_inserter( stringFib ),
+                    [prefix]( int i ) { return prefix + std::to_string( i ); } );
+
+            //REQUIRE_THAT( stringFib, Equals(expected) ); // fails as-is
+        }
+    }
+}
