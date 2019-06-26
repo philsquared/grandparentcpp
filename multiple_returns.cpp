b5c79a1c31fca5c1c179b65fa7b3de6a9d8be9ff
Phil nash <github@philnash.me>
2019-06-26 21:55:59 +0100

    Added examples for optional, variant, visit and string_view, as well as structured bindings

--- multiple_returns.cpp
+++ multiple_returns.cpp
@@ -1,4 +1,35 @@
-//
-// Created by Phil Nash on 2019-06-26.
-//
+#include <map>
+#include <iostream>
+#include "catch.hpp"
 
+TEST_CASE( "map insert" ) {
+
+    std::map<int, std::string> numberNames = {
+            { 1, "one" },
+            { 2, "two" },
+            { 3, "three "}
+        };
+
+    SECTION( "Full result type" ) {
+        std::pair<std::map<int, std::string>::const_iterator, bool> result = numberNames.insert({4, "four"});
+        REQUIRE( result.second == true );
+    }
+
+    SECTION( "auto result type" ) {
+        // using auto is cleaner, but even harder to reason about!
+        auto result2 = numberNames.insert({4, "four"});
+    }
+
+    SECTION( "structured bindings" ) {
+        // structured bindings let us break the result apart
+        auto[it, added] = numberNames.insert({4, "four"});
+        REQUIRE( added == true );
+
+        SECTION( "in for loops" ) {
+            for( auto const& [number, name] : numberNames )
+                std::cout << number << ": " << name << "\n";
+        }
+    }
+
+    // not just pairs - any structs/ classes with accessible members (inc tuples) - and arrays
+}
--- string_conversions.cpp
+++ string_conversions.cpp
@@ -1,4 +1,106 @@
-//
-// Created by Phil Nash on 2019-06-26.
-//
+#include "catch.hpp"
 
+#include <optional>
+#include <sstream>
+#include <variant>
+
+namespace Cpp17 {
+
+    // Writing a conversion from string to int without exceptions
+
+    // 1. take an out reference
+    [[nodiscard]] // must check return value
+    auto parse_int1( std::string_view sv, int& result ) -> bool {
+        std::stringstream ss;
+        ss << sv;
+        ss >> result;
+        return !ss.fail();
+    }
+
+    // 2. returning optional
+    auto parse_int2( std::string_view sv ) -> std::optional<int> {
+        int i;
+        std::stringstream ss;
+        ss << sv;
+        ss >> i;
+        if( !ss.fail() )
+            return i;
+        else
+            return {};
+    }
+
+    // 3. returning variant
+    auto parse_int3( std::string_view sv ) -> std::variant<int, std::domain_error> {
+        int i;
+        std::stringstream ss;
+        ss << sv;
+        ss >> i;
+        if( !ss.fail() )
+            return i;
+        else
+            return std::domain_error( "'" + std::string( sv ) + "' is not an integer" );
+    }
+}
+
+TEST_CASE( "String to int" ) {
+
+    SECTION( "C++ 11 stoul" ) {
+        REQUIRE( std::stoul( "7" ) == 7 ); // C++ 11
+
+        REQUIRE_THROWS( std::stoul( "Bakes7" ) ); // C++ 11
+    }
+
+    SECTION( "C++ 17" ) {
+
+        using namespace Cpp17;
+
+        SECTION( "out param" ) {
+            int i = 0;
+            parse_int1( "7", i ); // warning
+
+            REQUIRE( parse_int1( "7", i ) == true );
+            REQUIRE( i == 7 );
+
+            REQUIRE( parse_int1( "Blakes7", i ) == false );
+            // value of i is not defined
+        }
+
+        SECTION( "optional" ) {
+            REQUIRE( parse_int2( "7" ) == 7 );
+            REQUIRE( parse_int2( "Blakes7" ).has_value() == false );
+        }
+
+        SECTION( "variant" ) {
+            REQUIRE( std::get<int>( parse_int3( "7" ) ) == 7 );
+            REQUIRE( std::get<std::domain_error>( parse_int3( "Blakes7" ) ).what() == std::string( "'Blakes7' is not an integer" ) );
+        }
+
+        SECTION( "variant - visit" ) {
+
+            struct ExpectedIntVisitor {
+                int expected;
+
+                void operator()( int i ) {
+                    REQUIRE( i == expected );
+                }
+                void operator()( std::domain_error const& err ) {
+                    FAIL( "Expected " << expected << " but got error: " << err.what() );
+                }
+            };
+            struct ExpectedErrorVisitor {
+
+                std::string expectedMessage;
+
+                void operator()( int i ) {
+                    FAIL( "Expected error but got integer, " << i );
+                }
+                void operator()( std::domain_error const& err ) {
+                    REQUIRE( err.what() == expectedMessage );
+                }
+            };
+
+            std::visit( ExpectedIntVisitor{ 7 }, parse_int3( "7" ) );
+            std::visit( ExpectedErrorVisitor{ "'Blakes7' is not an integer" }, parse_int3( "Blakes7" ) );
+        }
+    }
+}
